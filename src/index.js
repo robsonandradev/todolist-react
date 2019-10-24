29c0888689ba2f990c1b65d02d4b0ce0b9a6d882
RobsonAndradev <robsom.s.a@gmail.com>
2019-10-24 15:52:39 -0300

    List all TODOs on render

--- src/index.js
+++ src/index.js
@@ -2,13 +2,48 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import './index.css';
-import App from './app';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class TodoList extends React.Component {
+  constructor( props ) {
+    super( props );
+    this.state = {
+      todoListApiHost: "http://localhost:8080",
+      todoItems: []
+    };
+  }
+  
+  getTodoItems() {
+    axios.get( `${this.state.todoListApiHost}/list` )
+      .then( res => {
+        const items = res.data;
+        this.setState({
+          todoItems: items
+        });
+      });
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  render() {
+    this.getTodoItems();
+    return (
+      <main role="main" className="container">
+        <div className="starter-template">
+          <h1> This is React and bootstrap working together </h1>
+          <ul>
+            {this.state.todoItems.map( item => 
+              <li key={item._id}>
+                <input type="checkbox" value={item.done} /> {item.name}
+              </li>
+            )}
+          </ul>
+        </div>
+      </main>
+    );
+  }
+}
+
+ReactDOM.render(
+  <TodoList />,
+  document.getElementById('root')
+);
