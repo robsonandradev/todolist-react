8569ecaad8afe7ba20d40a35a3fb193157b0c93b
RobsonAndradev <robsom.s.a@gmail.com>
2019-12-01 11:28:27 -0300

    Add more style to modal window and to add button

--- src/addItem.js
+++ src/addItem.js
@@ -4,39 +4,42 @@ const AddItem = ( props ) => {
   const newItemRef = React.createRef(null);
   return (
     <div className="sideBySide">
-      <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#modalExemplo">
-        Add Item
+      <button type="button" className="btn btn-primary" id="btnNewItem" data-toggle="modal" data-target="#newItemModal">
+        +
       </button>
 
-      <div className="modal fade" id="modalExemplo" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div className="modal fade" id="newItemModal" tabIndex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
         <div className="modal-dialog" role="document">
           <div className="modal-content">
             <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">Add New Item</h5>
+              <h5 className="modal-title" id="modalLabel">Add New Item</h5>
               <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
-            <div className="modal-body">
-
-              <input type="text"
-                id="newItem"
-                ref={ newItemRef }
-                onKeyDown={( event ) => {
-                  if( event.keyCode === 13 ){
+            <div className="modal-body container">
+              <div className="row">
+                <div className="col-1"></div>
+                <input type="text"
+                  className="col-7"
+                  id="newItem"
+                  ref={ newItemRef }
+                  onKeyDown={( event ) => {
+                    if( event.keyCode === 13 ){
+                      props.onClick( newItemRef.current.value );
+                      newItemRef.current.value = "";
+                    }
+                  }}
+                />
+                <div className="col-1"></div>
+                <button className="btn material-icons col-1" title="Add new item"
+                  onClick={ () => {
                     props.onClick( newItemRef.current.value );
                     newItemRef.current.value = "";
-                  }
-                }}
-              />
-              <button className="material-icons" title="Add new item"
-                onClick={ () => {
-                  props.onClick( newItemRef.current.value );
-                  newItemRef.current.value = "";
-                }}>
-                playlist_add
-              </button>
-
+                  }}>
+                  playlist_add
+                </button>
+              </div>
             </div>
           </div>
         </div>
